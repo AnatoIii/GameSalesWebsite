a91925d04007f15277a34c517ac8cee03eb2adc4
WeirdFish09 <kovtkoyura@gmail.com>
2020-05-10 15:59:41 +0300

    Base parsers structure

---
+++ Parsers/Parsers.Core/IParser.cs
@@ -0,0 +1,14 @@
+﻿using Parsers.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsers.Core
+{
+    public interface IParser
+    {
+        Task<IEnumerable<GameEntry>> ParsePlatform();
+        ParserSettings ParserSettings { get; }
+    }
+}
---
+++ Parsers/Parsers.Infrastructure/ParserService.cs
@@ -0,0 +1,47 @@
+﻿using Parsers.Core;
+using Parsers.Infrastructure;
+using Parsers.QueueClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsers.Infrastructure
+{
+    public class ParserService
+    {
+        private readonly IParser _parser;
+        private readonly ILogger _logger;
+        private readonly Client _client;
+        private bool IsAlive = true;
+        public ParserService(IParser parser, ILogger logger)
+        {
+            _parser = parser;
+            _logger = logger;
+            _client = new Client(logger, parser.ParserSettings.QueueSettings);
+        }
+
+        public async Task StartService()
+        {
+            while (IsAlive)
+            {
+                _logger.Log($"Process started - {DateTime.Now}");
+                var entities = await _parser.ParsePlatform();
+                _client.SendEntries(entities);
+                _logger.Log($"Process ended - {DateTime.Now}");
+                await Task.Delay(_parser.ParserSettings.PeriodBetweenParserActivations);
+
+            }
+        }
+
+        public void PauseService()
+        {
+            IsAlive = false;
+        }
+        public void ResumeService()
+        {
+            IsAlive = true;
+        }
+
+    }
+}
